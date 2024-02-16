48eb4ab72475694307b0f3683236cc34a99b4fd9
Lucas Amaro <lucash.96@hotmail.com>
2024-02-15 23:47:23 -0300

    Trabalhando com HttpClient nos documentos ts.

--- SpringBoot_CRUD_FullStack/FullStack_Front_End/Funcionarios/src/app/funcionario-lista/funcionario-lista.component.ts
+++ SpringBoot_CRUD_FullStack/FullStack_Front_End/Funcionarios/src/app/funcionario-lista/funcionario-lista.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Funcionario } from '../funcionario';
+import { FuncionarioService } from '../funcionario.service';
 
 @Component({
   selector: 'app-funcionario-lista',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './funcionario-lista.component.html',
   styleUrl: './funcionario-lista.component.css'
 })
@@ -13,23 +15,25 @@ export class FuncionarioListaComponent implements OnInit {
 
   funcionarios: Funcionario[] = [];
 
-  constructor() {}
+  constructor(private funcionarioService: FuncionarioService) {}
 
   ngOnInit(): void {
-    
-    this.funcionarios = [{
-      "id": 1,
-      "nome": "Lucas",
-      "sobrenome": "Herculano",
-      "email": "lucash.96@hotmail.com"
-    },
-    {
-      "id": 2,
-      "nome": "Juliana",
-      "sobrenome": "Araújo",
-      "email": "ju.s.araujo@hotmail.com"
-    }];
 
+    this.getFuncionarios();
+
+    /* Este código foi usado para testar se os métodos e rotas estavam devidamente configurados
+
+    this.funcionarios = [
+      {  "id": 1,  "nome": "Lucas",  "sobrenome": "Herculano", "email": "lucash.96@hotmail.com"  },
+      { "id": 2,  "nome": "Juliana",  "sobrenome": "Araújo",  "email": "ju.s.araujo@hotmail.com"}
+    ];
+    */
   }
 
+  private getFuncionarios() {
+    this.funcionarioService.getFuncionarioLista().subscribe(data => {
+      this.funcionarios = data;
+    })
+  }
+  
 }
---
+++ SpringBoot_CRUD_FullStack/FullStack_Front_End/Funcionarios/src/app/funcionario.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Funcionario } from './funcionario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  privatebaseURL= "http://localhost:8080/api/v1/Funcionarios"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getFuncionarioLista(): Observable<Funcionario[]> {
+    return this.httpClient.get<Funcionario[]>('${this.baseURL');
+  }
+
+}
