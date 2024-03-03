4b0e89388b9e9c1b148d70591b35b1454ab63d3c
Lucas Amaro <lucash.96@hotmail.com>
2024-03-02 21:12:12 -0300

    Criando o método de busca pelo ID

--- SpringBoot_CRUD_FullStack/FullStack_Back_End/Funcionarios/src/main/java/br/com/Funcionarios/controller/FuncionariosController.java
+++ SpringBoot_CRUD_FullStack/FullStack_Back_End/Funcionarios/src/main/java/br/com/Funcionarios/controller/FuncionariosController.java
@@ -3,13 +3,16 @@ package br.com.Funcionarios.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.Funcionarios.exception.ResourceNotFoundException;
 import br.com.Funcionarios.model.Funcionario;
 import br.com.Funcionarios.repository.FuncionarioRepository;
 
@@ -32,6 +35,14 @@ public class FuncionariosController {
 	@PostMapping("/Funcionarios")
 	public Funcionario createFuncionario(@RequestBody Funcionario funcionario) {
 		return funcionarioRepository.save(funcionario);
+	}
+	
+	// Pegando Funcionários do ID rest api
+	@GetMapping("/Funcionarios/{id}")
+	public ResponseEntity<Funcionario> getFuncionarioByID(@PathVariable Long id) {
+		Funcionario funcionario = funcionarioRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Funcionario não existe com o id: " + id));
+		return ResponseEntity.ok(funcionario);
 	}
 	
 }
